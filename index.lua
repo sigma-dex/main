718904213cd8cc7b7097be1464cb1b79975cf46f
1234server-elo <hghhhh.hhhhhh@interia.pl>
2024-11-14 15:57:09 +0100

    Update index.lua

--- index.lua
+++ index.lua
@@ -1,4 +1,4 @@
-local data = {["sigma-dex"] = loadstring(game:HttpGet("https://sigma-dex.github.io/main/script/1.lua"))()}
+local data = {["sigma-dexV1"] = loadstring(game:HttpGet("https://sigma-dex.github.io/main/script/1.lua"))()}
 local function run(name, bool)
   if data[name] and bool then
     loadstring(data[name])()
