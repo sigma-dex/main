949e8982b26533dbc9d803172932a1c748ab48a3
1234server-elo <hghhhh.hhhhhh@interia.pl>
2024-12-15 21:04:42 +0100

    Update index.lua

--- index.lua
+++ index.lua
@@ -1,9 +1,10 @@
+local url = "https://sigma-dex.github.io/main/script/"
 local data = {
-  ["sigma-dexV1"] = loadstring(game:HttpGet("https://sigma-dex.github.io/main/script/1.lua"))(),
-  ["Block-by-BlockV1"] = loadstring(game:HttpGet("https://sigma-dex.github.io/main/script/2.lua"))()
+  ["sigma-dexV1"] = loadstring(game:HttpGet(url.."1.lua"))(),
+  ["Block-by-BlockV1"] = loadstring(game:HttpGet(url.."2.lua"))()
 }
 local function run(name, bool)
   if data[name] and bool then
-    loadstring(data[name])()
+    data[name]()
   end
 end
